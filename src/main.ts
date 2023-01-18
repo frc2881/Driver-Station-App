0b72e7f0cbda3523f9c6ef9d9476f48f75874a18
Robert Tuttle <roberttuttle@users.noreply.github.com>
2023-01-17 21:56:23 -0600

    Implement native UI reload in dev mode (replacing third party util)

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { app, BrowserWindow } from "electron";
 import * as path from "path";
 import minimist from "minimist";
 import { fork, ChildProcess } from "child_process";
+import { watch } from "fs";
 import { 
   Configuration,
   AppArguments, 
@@ -17,6 +18,9 @@ class Main {
   }
 
   private _appServer!: ChildProcess;
+  private _appWindows = [] as BrowserWindow[];
+  private _abortController = new AbortController();
+  private _isDevMode = !app.isPackaged;
 
   private init = async (): Promise<void> => {
     const args = minimist(process.argv, {
@@ -33,10 +37,14 @@ class Main {
     this.createAppWindow(AppWindowType.HUD, { x: 0, y: 0 });
     this.createAppWindow(AppWindowType.DASHBOARD, { x: 30, y: 50 });
     this.createAppWindow(AppWindowType.DATA, { x: 60, y: 100 });
+
+    if (this._isDevMode) {
+      this.startUiAutoReload();
+    }
   }
 
   private createAppWindow = (appWindowType: AppWindowType, position: Position): void => {
-    const window = new BrowserWindow({
+    const appWindow = new BrowserWindow({
       title: `Driver Station - ${ appWindowType }`,
       width: 1280,
       height: 720,
@@ -47,11 +55,24 @@ class Main {
         webSecurity: false
       }
     });
-    window.menuBarVisible = false;
-    window.loadFile(
+    appWindow.menuBarVisible = false;
+    appWindow.loadFile(
       path.join(__dirname, "ui/index.html"), 
       { query: { "appWindowType": appWindowType } }
     );
+    this._appWindows.push(appWindow);
+  }
+
+  private startUiAutoReload = (): void => {
+    watch(
+      path.join(__dirname, "ui"), { 
+        recursive: true, 
+        signal: this._abortController.signal 
+      }).on("change", () => {
+        for (const appWindow of this._appWindows) {
+          appWindow.reload();
+        }
+    });
   }
 
   private onAppWillQuit = (): void => {
@@ -59,7 +80,8 @@ class Main {
   }
 
   private onAppQuit = (): void => {
-    if (!app.isPackaged) {
+    this._abortController.abort();
+    if (this._isDevMode) {
       console.log("All windows closed and application exited. Press [ Ctrl+C ] to terminate process.");
     }
   }
