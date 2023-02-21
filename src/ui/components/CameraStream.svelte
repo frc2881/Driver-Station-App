8ae5cec954adcacb58cc1fd6ed569df1a76d7773
Robert Tuttle <roberttuttle@users.noreply.github.com>
2023-02-20 22:01:04 -0600

    Move camera stream into common component

---
+++ src/ui/components/CameraStream.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import Video_02 from "carbon-pictograms-svelte/lib/Video_02.svelte";
+
+  export let stream: string;
+  export let isConnected: boolean;
+
+  const transparentPixelImage = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
+
+  let image: HTMLImageElement;
+
+  const reloadStream = (): void => {
+    image.src = transparentPixelImage;
+    image.src = stream;
+  }
+</script>
+
+<div class="cameraStream">
+  <div class="icon"><Video_02 class="placeholder" /></div>
+  <img 
+    bind:this={ image } 
+    on:error={ reloadStream } 
+    src={ isConnected ? stream : transparentPixelImage } 
+    alt="" />
+</div>
+
+<style lang="postcss">
+  .cameraStream {
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 960px;
+    height: 540px;
+
+    & img {
+      position: relative;
+      width: 100%;
+      height: 100%;
+
+      &::before, &::after {
+        display: none;
+      }
+    }
+
+    & .icon {
+      position: absolute;
+    }
+  }
+</style>
--- src/ui/views/Hud.svelte
+++ src/ui/views/Hud.svelte
@@ -1,25 +1,20 @@
 <script lang="ts">
-  import Video_02 from "carbon-pictograms-svelte/lib/Video_02.svelte";
   import { 
     Configuration,
 		NetworkTables
 	} from "../../common";
+  import CameraStream from "../components/CameraStream.svelte";
 
   export let networkTables: NetworkTables;
-
-  let leftCameraImage: HTMLImageElement;
-  let rightCameraImage: HTMLImageElement;
 </script>
 
 <main>
-  <div class="cameraStream">
-    <div class="icon"><Video_02 class="placeholder" /></div>
-    <img bind:this={ leftCameraImage } on:load={ (e) => { console.log(e); } }  on:error={ () => { const src = leftCameraImage.src; leftCameraImage.src = ""; leftCameraImage.src = src; } } src={ networkTables.isConnected ? Configuration.Settings.CAMERA_STREAMS.LEFT : "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" } alt="" />
-  </div>
-  <div class="cameraStream">
-    <div class="icon"><Video_02 class="placeholder" /></div>
-    <img bind:this={ rightCameraImage } on:load={ (e) => { console.log(e); } } on:error={ () => { const src = rightCameraImage.src; rightCameraImage.src = ""; rightCameraImage.src = src; } } src={ networkTables.isConnected ? Configuration.Settings.CAMERA_STREAMS.RIGHT : "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" } alt="" />
-  </div>
+  <CameraStream 
+    stream={ Configuration.Settings.CAMERA_STREAMS.LEFT }
+    isConnected={ networkTables.isConnected }  />
+  <CameraStream 
+    stream={ Configuration.Settings.CAMERA_STREAMS.RIGHT }
+    isConnected={ networkTables.isConnected }  />
 </main>
 
 <style lang="postcss">
@@ -29,28 +24,5 @@
     justify-content: center;
     width: 100vw;
     height: 100vh;
-  }
-
-  .cameraStream {
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 960px;
-    height: 540px;
-
-    & img {
-      position: relative;
-      width: 100%;
-      height: 100%;
-
-      &::before, &::after {
-        display: none;
-      }
-    }
-
-    & .icon {
-      position: absolute;
-    }
   }
 </style>
